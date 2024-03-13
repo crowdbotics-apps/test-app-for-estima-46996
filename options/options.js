ae3895fcd9234ce240a5acd9e89dbbc723ce5e59
Crowdbotics <team@crowdbotics.com>
2024-03-13 11:59:35 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "test-app-for-estima-46996",
+  url: "https://test-app-for-estima-46996.botics.co",
+  api: "https://test-app-for-estima-46996.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
